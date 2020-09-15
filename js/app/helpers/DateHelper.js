f7e274e0f64bb0bacf0d84862793db738a68b0f7
diogosfpinto <diogo.sfpinto@gmail.com>
2020-09-15 18:52:27 -0400

    Alterado metodo DateHelper para usar TemplateString e expressoes regulares

--- js/app/helpers/DateHelper.js
+++ js/app/helpers/DateHelper.js
@@ -1,10 +1,20 @@
 class DateHelper {
 
-    textoParaData(texto) {
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static textoParaData(texto) {
+
+        // Expressão regular
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+            throw new Error('Deve estar no formato YYYY-mm-dd');
+
         return new Date(...texto.split("-").map((item, indice) => item - indice % 2));
     }
 
-    dataParaTexto(data) {
-        return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+    static dataParaTexto(data) {
+        // Template String
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
 }
