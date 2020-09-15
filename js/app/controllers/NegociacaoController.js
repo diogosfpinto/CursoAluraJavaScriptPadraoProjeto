1865413cc32d2496dbf94b21ab6e136d3bd2328f
diogosfpinto <diogo.sfpinto@gmail.com>
2020-09-14 20:39:52 -0400

    busca valores do form para o controller

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,7 +1,19 @@
 class NegociacaoController {
+
+    constructor() {
+        
+        let $ = document.querySelector.bind(document);
+        this.inputData = $('#data') ;
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
     
     adiciona(event){
         event.preventDefault();
-        alert("Chamei ação no controler");
+        
+
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
     }
 }
