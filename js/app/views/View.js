a9f40ca1b50e7616428387ccd36eee028cd1478f
diogosfpinto <diogo.sfpinto@gmail.com>
2020-09-17 11:56:58 -0400

    Realizado herança para objetos da classe NegociacoesView e MensagemView origirando a classe View com métodos e atributos comuns entre essas classes

---
+++ js/app/views/View.js
@@ -0,0 +1,12 @@
+class View {
+
+    constructor(elemento) {
+        this._elemento = elemento;
+    }
+
+    _template(){}
+
+    update(model){
+        this._elemento.innerHTML = this._template(model);
+    }
+}
